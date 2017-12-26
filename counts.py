2e1d8644a5c38510f4a3a3892eacf4b6f061c1e1
redox96 <hannahoriginal7@gmail.com>
2017-12-26 18:07:32 +0100

    removed redundant code

--- counts.py
+++ counts.py
@@ -4,75 +4,17 @@ def count_stats(buffer, type):
     num_tot = 24 - len(buffer)
     counts_total = 0
 
-    if type == "Corner":
-        with open ("output_test_Corner.csv", "r", newline="") as letterlist:
-            reader = csv.reader(letterlist)
-            result_list = list(reader)
+    with open ("output_test_%s.csv"%(type), "r", newline="") as letterlist:
+        reader = csv.reader(letterlist)
+        result_list = list(reader)
 
-            for i in range(num_tot*(num_tot-1)):
-                counts_total = counts_total + int(result_list[i][2])
+        for i in range(num_tot*(num_tot-1)):
+            counts_total = counts_total + int(result_list[i][2])
 
-            #or .counts for result file
-            #print(counts_total)
-            return counts_total
+        #or .counts for result file
+        #print(counts_total)
+        return counts_total
 
-    elif type == "Edge":
-        with open ("output_test_Edge.csv", "r", newline="") as letterlist:
-            reader = csv.reader(letterlist)
-            result_list = list(reader)
-
-            for i in range(num_tot*(num_tot-1)):
-                counts_total = counts_total + int(result_list[i][2])
-
-            #print(counts_total)
-            return counts_total
-
-    elif type == "Center":
-        with open ("output_test_Center.csv", "r", newline="") as letterlist:
-            reader = csv.reader(letterlist)
-            result_list = list(reader)
-
-            for i in range(num_tot*(num_tot-1)):
-                counts_total = counts_total + int(result_list[i][2])
-
-            #print(counts_total)
-            return counts_total
-
-    elif type == "Wing":
-        with open ("output_test_Wing.csv", "r", newline="") as letterlist:
-            reader = csv.reader(letterlist)
-            result_list = list(reader)
-
-            for i in range(num_tot*(num_tot-1)):
-                counts_total = counts_total + int(result_list[i][2])
-
-            #print(counts_total)
-            return counts_total
-
-    elif type == "Midge":
-        with open ("output_test_Midge.csv", "r", newline="") as letterlist:
-            reader = csv.reader(letterlist)
-            result_list = list(reader)
-
-            for i in range(num_tot*(num_tot-1)):
-                counts_total = counts_total + int(result_list[i][2])
-
-            #print(counts_total)
-            return counts_total
-
-    elif type == "Tcenter":
-        with open ("output_test_Tcenter.csv", "r", newline="") as letterlist:
-            reader = csv.reader(letterlist)
-            result_list = list(reader)
-
-            for i in range(num_tot*(num_tot-1)):
-                counts_total = counts_total + int(result_list[i][2])
-
-            #print(counts_total)
-            return counts_total
-
-    else:
-        print("Error in counts")
 
 
 #cutoff_stats(["A","E","R"],"Corner")
--- cutoff.py
+++ cutoff.py
@@ -4,92 +4,18 @@ def cutoff_stats(cutoff, buffer, type):
     num_tot = 24 - len(buffer)
     above_cutoff = 0
 
-    if type == "Corner":
-        with open ("output_test_Corner.csv", "r", newline="") as letterlist:
-            reader = csv.reader(letterlist)
-            result_list = list(reader)
+    with open ("output_test_%s.csv"%(type), "r", newline="") as letterlist:
+        reader = csv.reader(letterlist)
+        result_list = list(reader)
 
-            for i in range(0,num_tot*(num_tot-1)-1):
-                if float(result_list[i][1]) > cutoff:
-                    above_cutoff = above_cutoff+1
-                else:
-                    continue
+        for i in range(0,num_tot*(num_tot-1)-1):
+            if float(result_list[i][1]) > cutoff:
+                above_cutoff = above_cutoff+1
+            else:
+                continue
 
-            print(above_cutoff)
-            return above_cutoff
-
-    elif type == "Edge":
-        with open ("output_test_Edge.csv", "r", newline="") as letterlist:
-            reader = csv.reader(letterlist)
-            result_list = list(reader)
-
-            for i in range(num_tot*(num_tot-1)):
-                if float(result_list[i][1]) > cutoff:
-                    above_cutoff = above_cutoff+1
-                else:
-                    continue
-
-            print(above_cutoff)
-            return above_cutoff
-
-    elif type == "Center":
-        with open ("output_test_Center.csv", "r", newline="") as letterlist:
-            reader = csv.reader(letterlist)
-            result_list = list(reader)
-
-            for i in range(num_tot*(num_tot-1)):
-                if float(result_list[i][1]) > cutoff:
-                    above_cutoff = above_cutoff+1
-                else:
-                    continue
-
-            print(above_cutoff)
-            return above_cutoff
-
-    elif type == "Wing":
-        with open ("output_test_Wing.csv", "r", newline="") as letterlist:
-            reader = csv.reader(letterlist)
-            result_list = list(reader)
-
-            for i in range(num_tot*(num_tot-1)):
-                if float(result_list[i][1]) > cutoff:
-                    above_cutoff = above_cutoff+1
-                else:
-                    continue
-
-            print(above_cutoff)
-            return above_cutoff
-
-    elif type == "Midge":
-        with open ("output_test_Midge.csv", "r", newline="") as letterlist:
-            reader = csv.reader(letterlist)
-            result_list = list(reader)
-
-            for i in range(num_tot*(num_tot-1)):
-                if float(result_list[i][1]) > cutoff:
-                    above_cutoff = above_cutoff+1
-                else:
-                    continue
-
-            print(above_cutoff)
-            return above_cutoff
-
-    elif type == "Tcenter":
-        with open ("output_test_Tcenter.csv", "r", newline="") as letterlist:
-            reader = csv.reader(letterlist)
-            result_list = list(reader)
-
-            for i in range(num_tot*(num_tot-1)):
-                if float(result_list[i][1]) > cutoff:
-                    above_cutoff = above_cutoff+1
-                else:
-                    continue
-
-            print(above_cutoff)
-            return above_cutoff
-
-    else:
-        print("Did you choose a cutoff?")
+        print(above_cutoff)
+        return above_cutoff
 
 
 #cutoff_stats(8, ["A","E","R"],"Corner")
